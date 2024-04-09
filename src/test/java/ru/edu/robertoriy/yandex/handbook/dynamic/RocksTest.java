68a27271f40854b7937ea0e103a24a1d2bc415be
robertoriy <holdenxhume@gmail.com>
2024-04-10 02:46:13 +0700

    dynamic programming: add tests

--- src/test/java/ru/edu/robertoriy/yandex/handbook/dynamic/RocksTest.java
+++ src/test/java/ru/edu/robertoriy/yandex/handbook/dynamic/RocksTest.java
@@ -16,8 +16,21 @@ final class RocksTest {
         "4, 5, Win",
         "6, 8, Loose"
     })
-    void testRocksGameNormalCase(int x, int y, String expected) {
+    void testRocksSimpleGame(int x, int y, String expected) {
         String actual = RocksUtils.play(new RocksUtils.Position(x, y));
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Тест: Продвинутые камни - (1, 0) | (0, 1) | (2, 0) | (0, 2) | (2, 1) | (1, 2)")
+    @CsvSource({
+        "4, 4, Loose",
+        "17, 72, Win",
+        "7, 5, Win"
+    })
+    void testRocksAdvancedGame(int x, int y, String expected) {
+        String actual = AdvancedRocksUtils.play(new AdvancedRocksUtils.Position(x, y));
 
         assertThat(actual).isEqualTo(expected);
     }
